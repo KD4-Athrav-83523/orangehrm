9a3f59f89e216ae20674a5f0b5774f266f64828a
unknown <apailwan53@gmail.com>
2026-08-16 17:22:12 +0530

    demo02.js

---
+++ demo02.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+console.log("This is a demo JavaScript file.");
+console.log("It demonstrates basic console logging in JavaScript.");
+console.log("You can log strings, numbers, and even objects.");
+console.log("For example, here's an object:", { name: "Alice", age: 30 });
+console.log("You can also log arrays:", [1, 2, 3, 4, 5]);
