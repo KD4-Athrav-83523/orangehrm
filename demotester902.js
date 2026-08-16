dccb4e61a0190589bbbd3f296c6106744cd8d836
unknown <apailwan53@gmail.com>
2026-08-16 21:46:49 +0530

    saved file uploding

---
+++ demotester902.js
@@ -0,0 +1,17 @@
+function generatePassword(length) {
+    const chars =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "abcdefghijklmnopqrstuvwxyz" +
+        "0123456789" +
+        "!@#$%^&*";
+
+    let password = "";
+
+
+    return password;
+}
+
+const password = generatePassword(12);
+
+console.log("Generated Password:", password);
+console.log("This is a test file for the OrangeHRM project.");
